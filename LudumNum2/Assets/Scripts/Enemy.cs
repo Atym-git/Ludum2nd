54d80b88a10b0eef019ab60d9a01fc63de6f4d4f
Atym-git <chuvachelaad@gmail.com>
2025-04-04 12:39:22 +0300

    Enemy done, camera needs a fix

--- LudumNum2/Assets/Scripts/Enemy.cs
+++ LudumNum2/Assets/Scripts/Enemy.cs
@@ -1,16 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _followSpeed = 0.05f;
+    [SerializeField] private float _moveSpeed = 0.05f;
+    private Rigidbody2D _rb2D;
 
-    private void OnTriggerStay2D(Collider2D player)
+    private float _maxXPosition;
+    private float _minXPosition;
+
+    private int _direction = 1;
+
+    private float _minTimeToTurn = 7.5f;
+    private float _maxTimeToTurn = 12.5f;
+
+    [SerializeField] private LayerMask _groundLayerMask;
+    [SerializeField] private LayerMask _playerLayerMask;
+
+    private void Start()
     {
-        if (player.GetComponent<PlayerInput>() != null)
+        _rb2D = GetComponent<Rigidbody2D>();
+        StartCoroutine(RandomTurn());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (LayerMaskUtil.ContainsLayer(_groundLayerMask, collision.gameObject))
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, _followSpeed);
+            //(Scale - 1) / 2 = delta for max & min values
+            _maxXPosition = collision.transform.position.x + ((collision.transform.lossyScale.x - 1) / 2);
+            _minXPosition = collision.transform.position.x - ((collision.transform.lossyScale.x - 1) / 2);
         }
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private IEnumerator RandomTurn()
+    {
+        while (true)
+        {
+            float timeToTurn = Random.Range(_minTimeToTurn, _maxTimeToTurn);
+            yield return new WaitForSeconds(timeToTurn);
+            _direction *= -1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        EnemyMove();
+    }
+    private void EnemyMove()
+    {
+        if (transform.position.x >= _maxXPosition || transform.position.x <= _minXPosition)
+        {
+            _direction *= -1;
+        }
+        _rb2D.velocity = new Vector2(_moveSpeed * _direction, _rb2D.velocity.y);
     }
 }
